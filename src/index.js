4f31611cff80052397d608023d4eda34730bf5e4
Gustavo Rana <gustavo.rana@cubos.io>
2020-01-03 17:38:47 -0300

    white sound

--- src/index.js
+++ src/index.js
@@ -2,18 +2,29 @@ import {createTimer} from './timer/index';
 import {updatedView} from './updateView/index';
 import { createButtonsListeners } from './buttonsListeners/index';
 import { updateWallpaper } from './images/images';
+import whitesoundPlayer from './whiteSound/index'
 
 var audio = new Audio('assets/timer.wav');
 audio.volume = 0.4;
 var jobMinutes = 25;
 var pauseMinutes = 5;
+const whiteSound = whitesoundPlayer();
 const timer = createTimer();
 const buttonsListener = createButtonsListeners();
 
 const buttonsActions = {
-    "job-timer": () => timer.play(jobMinutes * 60),
-    "pause-timer": () => timer.play(pauseMinutes * 60),
-    "stop": () => timer.stop(),
+    "job-timer": () => {
+        whiteSound.play();
+        timer.play(jobMinutes * 60);
+    },
+    "pause-timer": () => {
+        whiteSound.stop();
+        timer.play(pauseMinutes * 60);
+    },
+    "stop": () => {
+        whiteSound.stop();
+        timer.stop()
+    },
 }
 
 buttonsListener.onClick((buttonClicked)=>{
@@ -25,6 +36,7 @@ buttonsListener.onClick((buttonClicked)=>{
 timer.onChange(({status, seconds})=>{
     updatedView(seconds, status);
 
-    if(status === "end")
+    if(status === "end"){
         audio.play();
+    }
 });
--- src/whiteSound/index.js
+++ src/whiteSound/index.js
@@ -1,44 +1,21 @@
 function whitesoundPlayer() {
-    let config = {
-        volume: 0.4,
-        sounds: [
-            new Audio('assets/1a.ogg'),
-            new Audio('assets/4a.ogg'),
-            new Audio('assets/5a.ogg'),
-            new Audio('assets/6a.ogg'),
-            new Audio('assets/8a.ogg'),
-        ]
-    }
+    const audio = new Audio('assets/whitesound.wav');
+    audio.volume = 0.4;
 
     return {
         play() {
-            config.sounds.forEach(audio => {
-                audio.volume = config.volume;
-                audio.addEventListener('ended', function () {
-                    this.currentTime = 0;
-                    this.play();
-                }, false);
-                randomDelayPlay(audio);
-            })
+            audio.play();
+            audio.addEventListener('ended', function () {
+                this.currentTime = 0;
+                this.play();
+            }, false);
         },
         stop() {
-            config.sounds.forEach(audio => {
-                audio.pause();
-                audio.currentTime = 0;
-                audio.removeEventListener('ended', function () { });
-            })
+            audio.pause();
+            audio.currentTime = 0;
+            audio.removeEventListener('ended', function () { });
         }
     }
-}
-
-function randomDelayPlay(audio) {
-    const minMileSeconds = 500;
-    const maxMileSeconds = 8000;
-    const randomMileSeconds = Math.floor(Math.random() * (maxMileSeconds - minMileSeconds + 1) + minMileSeconds);
-
-    setTimeout(() => {
-        audio.play();
-    }, randomMileSeconds);
 }
 
 export default whitesoundPlayer;
